f88a1784d583f06ab671bc1d1c0e06738598b98f
Jesus Cebader <jesuscebader@Jesuss-MacBook-Pro.local>
2026-05-20 17:31:46 +0200

    chore: linter spaces rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const stylistic = require('@stylistic/eslint-plugin');
 const unusedImports = require('eslint-plugin-unused-imports');
 
 module.exports = defineConfig([
   expoConfig,
   {
     plugins: {
+      '@stylistic': stylistic,
       'unused-imports': unusedImports,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      '@stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      '@stylistic/no-trailing-spaces': 'error',
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
